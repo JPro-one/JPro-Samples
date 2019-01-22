2a6926ac7daf512ae8d17cb0134c1b906716320b
Florian Kirmaier <florian.kirmaier@gmail.com>
2019-01-22 19:12:55 +0400

    Updated the htmljpro sample

--- html-jpro/src/main/java/com/jpro/samples/htmljpro/HTMLJProSample.java
+++ html-jpro/src/main/java/com/jpro/samples/htmljpro/HTMLJProSample.java
@@ -1,5 +1,6 @@
 package com.jpro.samples.htmljpro;
 
+import com.jpro.webapi.HTMLView;
 import com.jpro.webapi.JProApplication;
 import javafx.application.Application;
 import javafx.scene.Parent;
@@ -10,6 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import org.apache.commons.text.StringEscapeUtils;
 
 public class HTMLJProSample extends JProApplication {
 
@@ -17,6 +19,10 @@ public class HTMLJProSample extends JProApplication {
     public void start(Stage primaryStage) throws Exception {
         VBox content = new VBox();
         content.getStyleClass().add("vbox");
+
+        Label headline = new Label("JPro");
+        headline.getStyleClass().add("headline");
+        content.getChildren().add(headline);
 
         content.getChildren().add(new Label("These controls are written in Java:"));
 
@@ -51,6 +57,27 @@ public class HTMLJProSample extends JProApplication {
             field.setText(s.substring(1,s.length() - 1).replace("\\\"","\""));
         });
         content.getChildren().add(field);
+
+
+        /*
+         * Let's add some HTML-Content
+         */
+        String content1 = "<h1>H1 with HTMLView!</h1>I have the same javascript and css-environment as the page.";
+        HTMLView htmlContent1 = new HTMLView(content1);
+        htmlContent1.setMinHeight(140);
+        htmlContent1.getStyleClass().add("html-view");
+        content.getChildren().add(htmlContent1);
+
+        /*
+         * Let's add some HTML-Content in a iframe
+         */
+        String content2 = "<h1>H1 in iframe!</h1>I'm inside an iframe. I have my own scope. I dont share the outer css files. I work more like WebView, because i behave more like an own browser window.";
+        String contentEncoded = StringEscapeUtils.escapeHtml4(content2);
+        String contentIframe = "<iframe frameborder=\"0\" style=\"width: 100%; height: 100%;\" srcdoc=\""+contentEncoded+"\"> </iframe>";
+        HTMLView htmlContent2 = new HTMLView(content2);
+        htmlContent2.setMinHeight(140);
+        htmlContent2.getStyleClass().add("html-view");
+        content.getChildren().add(htmlContent2);
 
 
         Scene scene = new Scene(content, Color.TRANSPARENT);
