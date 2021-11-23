0a32a027652444194b4c3bb4c56b0a893f89df14
floriankirmaier <florian.kirmaier@gmail.com>
2021-11-23 16:43:38 +0100

    some more pwa improvements

--- pwa/src/main/java/com/jpro/samples/pwa/PWAApp.java
+++ pwa/src/main/java/com/jpro/samples/pwa/PWAApp.java
@@ -17,28 +17,33 @@ public class PWAApp extends Application {
         CSSFX.start();
         PWAUtil pwa = new PWAUtil(primaryStage);
         VBox pin = new VBox();
+        VBox info = new VBox();
         pin.getStyleClass().add("vbox");
+        pin.getChildren().add(new Label("PWA Sample"));
+        pin.getChildren().add(info);
+
         Button installButton = new Button("Install as PWA");
         installButton.onActionProperty().set((e) -> {
             pwa.installPWA();
         });
-        installButton.setVisible(false);
-        pin.getChildren().add(new Label("PWA Sample"));
-        pin.getChildren().add(installButton);
+
         Scene scene = new Scene(new StackPane(pin));
         scene.getStylesheets().add("/com/jpro/samples/pwa/app.css");
         primaryStage.setScene(scene);
         primaryStage.show();
 
         pwa.installed().addListener((p,o,n) -> {
-            installButton.setVisible(false);
+            info.getChildren().clear();
             n.ifPresent(installed -> {
                 if(installed) {
-                    pin.getChildren().add(new Label("The App is already installed!"));
+                    info.getChildren().add(new Label("Your browser doesn't support PWA, or the App is already installed"));
                 } else {
-                    installButton.setVisible(true);
+                    info.getChildren().add(installButton);
                 }
             });
+            if(n.isEmpty()) {
+                info.getChildren().add(new Label("Your browser doesn't support PWA!"));
+            }
         });
     }
 }
