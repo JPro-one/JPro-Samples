d9896a7960eb9025786d732894ce1ada343025e8
floriankirmaier <florian.kirmaier@gmail.com>
2022-11-02 18:50:12 +0100

    Added multifilehandler sample

--- README.md
+++ README.md
@@ -9,17 +9,18 @@ This project contains multiple samples for [jpro, which enables javafx in the we
 Samples:
 
 
-  Sample | description | Demo
-  -------|-------------|-------
-  popups | Shows how to use popups with JPro. | [link](https://www.jfx-ensemble.com/sample/jpro-samples/PopupsApp)
-  filehandler | Shows how to upload and download files. |  [link](https://www.jfx-ensemble.com/sample/jpro-samples/FileHandlerApp)
-  html-jpro | A small sample, which shows how to use the WebAPI to communicate between JPro and a html-site. | ---
-  native-scrolling | A small sample on how to use native scrolling. | ---
-  website | A minimalistic website with multiple pages. It uses our library [jpro-web](https://github.com/Sandec/jpro-web) | ---
-  auth0 | Login with [Auth0](https://auth0.com/). Read the [readme](https://github.com/JPro-one/JPro-Samples/tree/master/auth0) for more details. | ---
-  suneditor | Example on how to integrate a html richtext editor - [suneditor]() in this case. | [link](https://www.jfx-ensemble.com/sample/jpro-samples/JPro_Suneditor)
-  gps | Shows how to retrieve gps location and use it to show a map. | [link](https://www.jfx-ensemble.co/sample/jpro-samples/JPro_GeoLocation)
-  pwa | Install your Website as a desktop/mobile app! Read the [readme](https://github.com/JPro-one/JPro-Samples/tree/master/pwa) for more details.
+  Sample | description                                                                                                                                 | Demo
+  -------|---------------------------------------------------------------------------------------------------------------------------------------------|-------
+  popups | Shows how to use popups with JPro.                                                                                                          | [link](https://www.jfx-ensemble.com/sample/jpro-samples/PopupsApp)
+  multifilehandler | Shows how to upload and download mltiple files.                                                                                             |  ---
+  filehandler | Shows how to upload and download files.                                                                                                     |  [link](https://www.jfx-ensemble.com/sample/jpro-samples/FileHandlerApp)
+  html-jpro | A small sample, which shows how to use the WebAPI to communicate between JPro and a html-site.                                              | ---
+  native-scrolling | A small sample on how to use native scrolling.                                                                                              | ---
+  website | A minimalistic website with multiple pages. It uses our library [jpro-web](https://github.com/Sandec/jpro-web)                              | ---
+  auth0 | Login with [Auth0](https://auth0.com/). Read the [readme](https://github.com/JPro-one/JPro-Samples/tree/master/auth0) for more details.     | ---
+  suneditor | Example on how to integrate a html richtext editor - [suneditor]() in this case.                                                            | [link](https://www.jfx-ensemble.com/sample/jpro-samples/JPro_Suneditor)
+  gps | Shows how to retrieve gps location and use it to show a map.                                                                                | [link](https://www.jfx-ensemble.co/sample/jpro-samples/JPro_GeoLocation)
+  pwa | Install your Website as a desktop/mobile app! Read the [readme](https://github.com/JPro-one/JPro-Samples/tree/master/pwa) for more details. 
 
 
 
@@ -28,6 +29,7 @@ Samples:
 
 ```
 ./gradlew popups:jproRun
+./gradlew multifilehandler:jproRun
 ./gradlew filehandler:jproRun
 ./gradlew html-jpro:jproRun
 ./gradlew native-scrolling:jproRun
---
+++ multifilehandler/src/main/java/com/jpro/samples/multifilehandler/MultiFileHandlerApp.java
@@ -0,0 +1,146 @@
+package com.jpro.samples.multifilehandler;
+
+import com.jpro.webapi.JProApplication;
+import com.jpro.webapi.WebAPI;
+import fr.brouillard.oss.cssfx.CSSFX;
+import javafx.application.Application;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class MultiFileHandlerApp extends JProApplication {
+
+    @Override
+    public void start(javafx.stage.Stage primaryStage) throws Exception {
+        CSSFX.start();
+        javafx.scene.Scene scene = new javafx.scene.Scene(new MultiFileHandler());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    class MultiFileHandler extends VBox {
+
+        List<WebAPI.JSFile> jsFiles = new ArrayList<>();
+        public MultiFileHandler() {
+            getStylesheets().add(getClass().getResource("/com/jpro/samples/multifilehandler/multifilehandler.css").toExternalForm());
+            getStyleClass().add("vbox");
+
+            Label label = new Label("MultiFileHandler");
+            label.getStyleClass().add("mf-title");
+            getChildren().add(label);
+
+            Label label2 = new Label("Drop Files to upload them");
+            label2.getStyleClass().add("mf-description");
+            getChildren().add(label2);
+
+            VBox filesVBox = new VBox();
+            filesVBox.getStyleClass().add("mf-files");
+            ScrollPane filesScrollPane = new ScrollPane(filesVBox);
+            filesScrollPane.setFitToWidth(true);
+            StackPane filesContainer = new StackPane(filesScrollPane);
+            filesContainer.getStyleClass().add("mf-files-container");
+            getChildren().add(filesContainer);
+
+
+            Button downloadButton = new Button("Download as zip");
+            downloadButton.getStyleClass().add("mf-download-button");
+            getChildren().add(downloadButton);
+
+            downloadButton.setOnAction(e -> {
+
+                List<CompletableFuture<File>> filesF = jsFiles.stream().map(f -> f.getUploadedFileFuture()).collect(Collectors.toList());
+                CompletableFuture.allOf(filesF.toArray(new CompletableFuture[filesF.size()])).thenRun(() -> {
+                    List<File> files = filesF.stream().map(f -> f.join()).collect(Collectors.toList());
+                    // Create a zip file from the files
+                    File zipFile = new File("download.zip");
+                    try {
+                        createZipFile(zipFile, files);
+                        // Download the zip file
+                        getWebAPI().downloadURL(zipFile.toURI().toURL());
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                });
+            });
+
+
+
+            WebAPI.getWebAPI(this, webAPI -> {
+                WebAPI.MultiFileUploader fuploader = webAPI.makeMultiFileUploadNode(filesContainer);
+                fuploader.fileDragOverProperty().addListener((o,oldV,newV) -> {
+                    if(newV) {
+                        filesContainer.getStyleClass().add("file-drag");
+                    } else {
+                        if(filesContainer.getStyleClass().contains("file-drag")) {
+                            filesContainer.getStyleClass().remove("file-drag");
+                        }
+                    }
+                });
+                fuploader.setSelectFileOnClick(true);
+                fuploader.setSelectFileOnDrop(true);
+                fuploader.setOnFilesSelected(files -> {
+                    for (WebAPI.JSFile file : files) {
+                        file.uploadFile();
+                        JSFileInfoCell cell = new JSFileInfoCell(file);
+                        filesVBox.getChildren().add(cell);
+                        jsFiles.add(file);
+                    }
+                });
+            });
+        }
+    }
+
+    class JSFileInfoCell extends HBox {
+        JSFileInfoCell(WebAPI.JSFile jsfile) {
+            getStyleClass().add("mf-file-cell");
+            Label label = new Label(jsfile.getFilename());
+            getChildren().add(label);
+
+            ProgressBar progressBar = new ProgressBar();
+            getChildren().add(progressBar);
+            progressBar.progressProperty().bind(jsfile.progressProperty());
+
+            // Size
+            Label sizeLabel = new Label();
+            getChildren().add(sizeLabel);
+            long size = jsfile.getFileSize();
+            // in kb or mb
+            if(size > 1024*1024) {
+                sizeLabel.setText(String.format("%.2f MB", size/(1024.0*1024.0)));
+            } else if(size > 1024) {
+                sizeLabel.setText(String.format("%.2f KB", size/(1024.0)));
+            } else {
+                sizeLabel.setText(String.format("%d B", size));
+            }
+
+
+        }
+    }
+
+    static void createZipFile(File zipFile, List<File> files) throws IOException {
+        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFile))) {
+            for (File file : files) {
+                ZipEntry zipEntry = new ZipEntry(file.getName());
+                zos.putNextEntry(zipEntry);
+                Files.copy(file.toPath(), zos);
+                zos.closeEntry();
+            }
+        }
+    }
+}
