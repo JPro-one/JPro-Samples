4e0d7bb058e611e212f25b52bba4d965e31a5079
Besmir Beqiri <bb@sandec.de>
2023-08-17 12:06:00 +0200

    Apply API changes regarding the update of the dependencies

--- auth0/src/main/java/com/jpro/samples/auth0/auth/AuthUtil.java
+++ auth0/src/main/java/com/jpro/samples/auth0/auth/AuthUtil.java
@@ -1,28 +1,19 @@
 package com.jpro.samples.auth0.auth;
 
-
 import com.auth0.client.auth.AuthAPI;
-import com.auth0.json.auth.TokenHolder;
-import com.auth0.json.auth.UserInfo;
 import com.auth0.jwk.JwkProvider;
 import com.auth0.jwk.JwkProviderBuilder;
-import com.auth0.jwk.UrlJwkProvider;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.net.CustomRequest;
-import com.auth0.net.TokenRequest;
-import com.jpro.webapi.WebAPI;
 
 public class AuthUtil {
     static String CLIENT_ID = "???";
     static String CLIENT_SECRET = "???";
     static String DOMAIN = "???";
 
-    AuthAPI client = null;
+    AuthAPI client;
 
     public AuthUtil() {
         JwkProvider jwkProvider = new JwkProviderBuilder(DOMAIN).build();
-        client = new AuthAPI(DOMAIN,CLIENT_ID,CLIENT_SECRET);
+        client = new AuthAPI(DOMAIN, CLIENT_ID, CLIENT_SECRET);
     }
 
     public String authURL() {
@@ -39,15 +30,14 @@ public class AuthUtil {
     public String verify(String code) {
         System.out.println("Got code: " + code);
         try {
-            CustomRequest<TokenHolder> tokenRequest = client.exchangeCode(code,"http://localhost:8080/");
-            TokenHolder token = tokenRequest.execute();
+            var tokenRequest = client.exchangeCode(code,"http://localhost:8080/");
+            var token = tokenRequest.execute();
+            var userInfo = client.userInfo(token.toString()).execute();
 
-            UserInfo info = client.userInfo(token.getAccessToken()).execute();
-
-            return (String) info.getValues().get("email");
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
+            return String.valueOf(userInfo.getBody().getValues().get("email"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
         }
     }
 }
