e68257ca7109a6fe09b830a53669b54c3fdf7223
Besmir Beqiri <bb@sandec.de>
2023-08-17 12:10:58 +0200

    Update the routing API to the latest version

--- website/src/main/java/com/jpro/samples/website/HelloJProApp.java
+++ website/src/main/java/com/jpro/samples/website/HelloJProApp.java
@@ -3,26 +3,21 @@ package com.jpro.samples.website;
 
 import com.jpro.samples.website.page.LandingPage;
 import com.jpro.samples.website.page.SubPage;
-import com.jpro.web.Redirect;
-import com.jpro.web.WebApp;
-import javafx.stage.Stage;
+import one.jpro.routing.Route;
+import one.jpro.routing.RouteApp;
 
-public class HelloJProApp extends WebApp {
-    HelloJProApp(Stage stage) {
-        super(stage);
+import static one.jpro.routing.RouteUtils.get;
+import static one.jpro.routing.RouteUtils.redirect;
 
-        getStylesheets().add("/com/jpro/samples/website/css/website.css");
+public class HelloJProApp extends RouteApp {
 
-        addRouteJava((s) -> {
-            if(s.equals("") || s.equals("/")) {
-                return new Redirect("/?page=main");
-            } else if(s.equals("/?page=main")) {
-                return new LandingPage();
-            } else if(s.equals("/?page=sub")) {
-                return new SubPage();
-            } else {
-                return null;
-            }
-        });
+    @Override
+    public Route createRoute() {
+        getScene().getStylesheets().add("/com/jpro/samples/website/css/website.css");
+
+        return Route.empty()
+                .and(redirect("/", "/?page=main"))
+                .and(get("/?page=main", request -> new LandingPage()))
+                .and(get("/?page=sub", request -> new SubPage()));
     }
 }
