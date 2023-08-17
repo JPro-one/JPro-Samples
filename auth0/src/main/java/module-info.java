2f57ef9463afd15cb49b5ca803fe5723921a5b68
Besmir Beqiri <bb@sandec.de>
2023-08-17 12:06:00 +0200

    Apply API changes regarding the update of the dependencies

--- auth0/src/main/java/module-info.java
+++ auth0/src/main/java/module-info.java
@@ -4,6 +4,8 @@ open module JPro.Samples.auth0.main {
     requires javafx.controls;
     requires com.fasterxml.jackson.databind;
     requires com.auth0.jwt;
+    requires auth0;
+    requires jwks.rsa;
 
     exports com.jpro.samples.auth0;
 }
