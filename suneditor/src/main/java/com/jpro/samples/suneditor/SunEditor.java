9be73d92a64cfe375f416559647e93b05d94fd7d
floriankirmaier <florian.kirmaier@gmail.com>
2023-08-17 17:36:40 +0400

    reworked the suneditor

--- suneditor/src/main/java/com/jpro/samples/suneditor/SunEditor.java
+++ suneditor/src/main/java/com/jpro/samples/suneditor/SunEditor.java
@@ -1,6 +1,7 @@
 package com.jpro.samples.suneditor;
 
 import com.jpro.webapi.HTMLView;
+import com.jpro.webapi.JSVariable;
 import com.jpro.webapi.WebAPI;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
@@ -10,69 +11,58 @@ import org.json.JSONTokener;
 
 public class SunEditor extends StackPane {
 
-    static int idCounter = 0;
-    static String getNewId() {
-        idCounter += 1;
-        return "suneditor_" + idCounter;
-    }
 
     HTMLView htmlView = new HTMLView();
 
     private WebAPI webAPI = null;
-    String id;
 
     StringProperty textProperty = new SimpleStringProperty("");
 
     private String elementContent = textProperty.get();
 
+    JSVariable sunElem;
+    JSVariable callback;
+
     SunEditor() {
         getChildren().add(htmlView);
         htmlView.setBlockKeyboardInput(true);
-        id = getNewId();
         setOnMouseClicked(e -> requestFocus());
         WebAPI.getWebAPI(this, webAPI -> {
             this.webAPI = webAPI;
             webAPI.loadCSSFile(getClass().getResource("/com/jpro/samples/suneditor/http/css/suneditor.min.css"));
             webAPI.loadJSFile(getClass().getResource("/com/jpro/samples/suneditor/http/js/suneditor.min.js"));
             webAPI.loadJSFile(getClass().getResource("/com/jpro/samples/suneditor/http/js/en.js"));
-            htmlView.setContent("<textarea id=\"" + id + "\">"+textProperty.get()+"</textarea>");
-            Platform.runLater(() -> {
 
-                webAPI.executeScript("var element = document.getElementById('"+id+"');" +
-                        "console.log('element: ' + element);" +
-                        "var editor = SUNEDITOR.create(element,{\n" +
-                        "});" +
-                        "jpro."+id+"=editor;");
+            htmlView.setContent("<textarea>"+textProperty.get()+"</textarea>");
 
+            JSVariable textarea = webAPI.getHTMLViewElement(htmlView);
 
-                webAPI.registerJavaFunction("callback1_"+id, str -> {
-                    JSONTokener tockener = new JSONTokener(str);
-                    tockener.nextClean();
-                    String str2 = tockener.nextString('"');
-                    elementContent = str2;
-                    textProperty.set(str2);
+            sunElem = webAPI.executeScriptWithVariable("SUNEDITOR.create(" + textarea.getName() + ",{});");
 
-                    System.out.println("Got str: " + str2);
-                });
-                webAPI.executeScript("jpro."+id+".onChange = function(contents,core){" +
-                        "  jpro.callback1_"+id+"(contents);" +
-                        "};");
-                textProperty.addListener((ps,os,ns) -> {
-                    if(elementContent != ns) {
-                        String script = "jpro."+id+".setContents(\""+ns.replace("\\","\\\\").replace("\"","\\\"")+"\");";
-                        webAPI.executeScript(script);
-                    }
-                });
-
-                widthProperty().addListener((p,o,n) -> updateWH());
-                heightProperty().addListener((p,o,n) -> updateWH());
-                updateWH();
+            callback = webAPI.registerJavaFunction(str -> {
+                JSONTokener tockener = new JSONTokener(str);
+                tockener.nextClean();
+                String str2 = tockener.nextString('"');
+                elementContent = str2;
+                textProperty.set(str2);
             });
+            webAPI.executeScript(sunElem.getName()+".onChange = function(contents,core){" +
+                    "  " + callback.getName()+"(contents);" +
+                    "};");
+            textProperty.addListener((ps,os,ns) -> {
+                if(elementContent != ns) {
+                    String script = sunElem.getName()+".setContents(\""+ns.replace("\\","\\\\").replace("\"","\\\"")+"\");";
+                    webAPI.executeScript(script);
+                }
+            });
+
+            widthProperty().addListener((p,o,n) -> updateWH(sunElem));
+            heightProperty().addListener((p,o,n) -> updateWH(sunElem));
+            updateWH(sunElem);
         });
     }
 
-    private void updateWH() {
-        webAPI.executeScript("var element = document.getElementById('"+id+"');" +
-                "jpro."+id+".setOptions({height: " + this.getHeight() + ", width: " + this.getWidth() + "});");
+    private void updateWH(JSVariable sunElem) {
+        webAPI.executeScript(sunElem.getName()+".setOptions({height: " + this.getHeight() + ", width: " + this.getWidth() + "});");
     }
 }
